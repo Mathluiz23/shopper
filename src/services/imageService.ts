9d2fb7fe4697598134a820d71693856a682a3eab
Mathluiz23 <matheus_luizs@hotmail.com>
2024-09-04 22:10:48 -0300

    :boom:

--- src/services/imageService.ts
+++ src/services/imageService.ts
@@ -13,21 +13,3 @@ export const convertImageToBase64 = (filePath: string): string => {
   const imageBuffer = fs.readFileSync(filePath);
   return imageBuffer.toString('base64');
 };
-
-
-
-// import axios from 'axios';
-// import fs from 'fs';
-
-// export const downloadImage = async (url: string, outputPath: string) => {
-//   const response = await axios({
-//     url,
-//     responseType: 'arraybuffer',
-//   });
-//   fs.writeFileSync(outputPath, response.data);
-// };
-
-// export const convertImageToBase64 = (filePath: string): string => {
-//   const imageBuffer = fs.readFileSync(filePath);
-//   return imageBuffer.toString('base64');
-// };
